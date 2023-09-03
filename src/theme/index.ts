9d428dd26f88316aeeb310d15c302328e18516e9
Lucas <lucasvieirasilva.lpta@gmail.com>
2023-09-03 16:10:14 -0300

    refactor: new theme file scheme

---
+++ src/theme/index.ts
@@ -0,0 +1,21 @@
+export const THEME = {
+    COLORS: {
+      TEXT: '#FFFFFF',
+    },
+  
+    FONT_FAMILY: {
+      REGULAR: 'UbuntuRegular',
+      BOLD: 'UbuntuBold',
+      MEDIUM: 'UbuntuMedium'
+    },
+
+    FONT_SIZE: {
+      SM: 14,
+      MD: 16,
+      LG: 24
+    },
+
+    SPACING: {
+      GAP: 8,
+    }
+  };
